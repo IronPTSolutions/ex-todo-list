c353f77b31a7e8f3924b9596f6e7c9829a4dbfe1
Giancarlo Patti <gpatti.11@gmail.com>
2022-06-25 14:17:01 +0200

    first commit

--- config/routes.config.js
+++ config/routes.config.js
@@ -1,6 +1,17 @@
 const express = require("express");
 const router = express.Router();
 
+const misc = require('../controllers/misc.controller');
+const tasks = require('../controllers/tasks.controller')
+
+router.get('/', misc.home);
+
+router.get('/tasks', tasks.list);
+
+router.get('/tasks/new', tasks.new)
+router.get('/tasks/:id', tasks.detail)
+router.post('/tasks', tasks.create)
+
 // TODO: GET /tasks -> render task list with filters
 // TODO: GET /tasks/:id -> render task detail
 // TODO: GET /tasks/new -> render task form
--- controllers/tasks.controller.js
+++ controllers/tasks.controller.js
@@ -3,23 +3,35 @@ const Task = require("../services/tasks.service");
 module.exports.list = (req, res, next) => {
   // TODO: use Task.find() to get all tasks and render tasks list
   // Task.find() returns a promise that resolves with task list
-  res.send("TODO");
+  Task.find()
+    .then(tasks => res.render('tasks/list', { tasks }))
+    .catch(err => console.log(err))
 };
 
 module.exports.detail = (req, res, next) => {
   // TODO: use Task.findById(id) to get task by id and render task detail
   // Task.findById() returns a promise that resolves with task detail (or undefined if it does not exist)
-  res.send("TODO");
+  const { id } = req.params;
+  Task.findById(id)
+    .then(tasks => res.render('tasks/detail', { tasks }))
+    .catch(error => next(error));
 };
 
 module.exports.create = (req, res, next) => {
   // TODO: use Task.create({...}) to create a new Task and redirect to list
   // Task.create() returns a promise that resolves with created task detail
-  res.send("TODO");
+  Task.create({ tasks })
+
 };
 
 module.exports.delete = (req, res, next) => {
   // TODO: use Task.findByIdAndDelete(id) to delete a task and redirect to list
   // Task.findByIdAndDelete() returns a promise that resolves with undefined
   res.send("TODO");
+};
+
+module.exports.new = (req, res, next) => {
+  // TODO: use Task.findByIdAndDelete(id) to delete a task and redirect to list
+  // Task.findByIdAndDelete() returns a promise that resolves with undefined
+  res.render('tasks/new');
 };
