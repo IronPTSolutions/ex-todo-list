bdf3d0ce7defb06f89e3c52770b1c25167edd36a
Iván Rodríguez <82571096+Ivan180687@users.noreply.github.com>
2022-06-25 17:07:16 +0200

    working

--- controllers/tasks.controller.js
+++ controllers/tasks.controller.js
@@ -1,29 +1,43 @@
 const Task = require("../services/tasks.service");
 
 module.exports.list = (req, res, next) => {
-  Task.find()
-    .then(task => {
-      res.render('tasks/list', { task })
-    })
+  Task.find().then((tasks) => {
+    res.render("tasks/list", { tasks });
+  });
   // TODO: use Task.find() to get all tasks and render tasks list
   // Task.find() returns a promise that resolves with task list
-  res.send("TODO");
+  //res.send("TODO");
 };
 
 module.exports.detail = (req, res, next) => {
+  Task.findById(req.params.id).then((task) => {
+    res.render("tasks/detail", { task });
+  });
   // TODO: use Task.findById(id) to get task by id and render task detail
   // Task.findById() returns a promise that resolves with task detail (or undefined if it does not exist)
-  res.send("TODO");
+  //res.send("TODO");
+};
+
+module.exports.new = (req, res, next) => {
+  res.render("tasks/new");
 };
 
 module.exports.create = (req, res, next) => {
+  const data = req.body;
+
+  Task.create(data).then((task) => {
+    res.redirect("/tasks");
+  });
   // TODO: use Task.create({...}) to create a new Task and redirect to list
   // Task.create() returns a promise that resolves with created task detail
-  res.send("TODO");
+  //res.send("TODO");
 };
 
 module.exports.delete = (req, res, next) => {
+  Task.findByIdAndDelete(req.params.id).then(() => {
+    res.redirect("/tasks");
+  });
   // TODO: use Task.findByIdAndDelete(id) to delete a task and redirect to list
   // Task.findByIdAndDelete() returns a promise that resolves with undefined
-  res.send("TODO");
+  //res.send("TODO");
 };
