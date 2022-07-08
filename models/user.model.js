4aee68b269320ff4aa4b2176026a9b0c0ddfa493
Carlos del Prado <carlos.delpradomota@telefonica.com>
2022-07-08 11:52:10 +0100

    fix: user password regex

--- models/user.model.js
+++ models/user.model.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const EMAIL_PATTERN = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-const PW_PATTERN = /.*{8,}/;
+const PW_PATTERN = /^.{8,}$/;
 
 const userSchema = new Schema(
   {
