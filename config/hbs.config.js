f0132f6b7c1cad02aefc60c138d2d16d58f06563
Carlos del Prado <carlos.delpradomota@telefonica.com>
2022-08-02 22:11:33 +0200

    feat: google maps inegration

--- config/hbs.config.js
+++ config/hbs.config.js
@@ -1,3 +1,22 @@
 const hbs = require("hbs");
 
 hbs.registerPartials(__dirname + "/../views/partials");
+
+hbs.registerHelper('markers', function(tasks) {
+  const markers = tasks.reduce((markers, task) => {
+    if (task.location?.coordinates) {
+      const [lng, lat] = task.location?.coordinates;
+      markers.push({ lng, lat, title: task.title })
+    }
+    return markers;
+  }, []);
+  return new hbs.SafeString(`<script>const gMarkers = ${JSON.stringify(markers)}</script>`);
+})
+
+hbs.registerHelper('hasLocation', function(task, options) {
+  if (task.location?.coordinates) {
+    return options.fn(this);
+  } else {
+    return options.inverse(this);
+  }
+});
--- public/js/g-places.js
+++ public/js/g-places.js
@@ -1,6 +1,34 @@
 function initGooglePlaces() {
   console.log('Places script loaded');
+  initPlacesSearchBar(); 
+  initGMap(); 
+}
 
+function initGMap() {
+  const mapContainer = document.querySelector('.g-map');
+  if (mapContainer) {
+    const center = { lat: -25.344, lng: 131.031 }
+    const map = new google.maps.Map(mapContainer, {
+      zoom: 7,
+      center: center,
+    });
+
+    if (gMarkers) {
+      gMarkers.forEach(({title, lng, lat}) => {
+        const marker = new google.maps.Marker({
+          position: { lng, lat },
+          map: map,
+          title
+        });
+        map.setCenter(marker.getPosition());
+      })
+    }
+
+    
+  }
+}
+
+function initPlacesSearchBar() {
   const input = document.querySelector(".g-places-finder");
   if (input) {
     const options = {
